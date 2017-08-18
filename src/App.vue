5817c2dab4fa130aa3f45cebc1c6c45f66233516
Scozzi <WestonJones87@gmail.com>
2017-08-18 16:50:33 -0400

    Initial Upload

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <div class="ui left vertical inverted labeled icon sidebar menu">
+    <a class="item">
+        <i class="home icon"></i>
+        Home
+    </a>
+    <a class="item">
+        <i class="block layout icon"></i>
+        Topics
+    </a>
+    <a class="item">
+        <i class="smile icon"></i>
+        Friends
+    </a>
+    <a class="item">
+        <i class="calendar icon"></i>
+        History
+    </a>
+    <a class="item">
+        <i class="mail icon"></i>
+        Messages
+    </a>
+    <a class="item">
+        <i class="chat icon"></i>
+        Discussions
+    </a>
+    <a class="item">
+        <i class="trophy icon"></i>
+        Achievements
+    </a>
+    <a class="item">
+        <i class="shop icon"></i>
+        Store
+    </a>
+    <a class="item">
+        <i class="settings icon"></i>
+        Settings
+    </a>
+  </div>
+  <div class="pusher">
+    <!-- Site content !-->
+    <img class="sidebar-opener" src="./assets/logo.png">
+    <router-view></router-view>
+
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'This is not the message you were looking for!'
+    }
+  },
+  mounted: function () {
+    console.log("MOUNTED");
+    this.initializeSemantic();
+    this.setupLogoSidebarOpener();
+  },
+  ready: function () {
+    console.log("READY");
+    this.initializeSemantic();
+    this.setupLogoSidebarOpener();
+  },
+  methods: {
+    initializeSemantic: function () {
+      console.log($(this.$el));
+      // Update elements to match what is needed by Semantic.js
+      $('#app .ui.sidebar').sidebar({context:$('#app')})
+      .sidebar('setting', 'transition', 'overlay')
+      .sidebar('toggle');
+    },
+    setupLogoSidebarOpener: function () {
+      var swipeMenu = $('#app .ui.sidebar');
+      $('.sidebar-opener').click(function () {
+        swipeMenu.sidebar('toggle');
+      })
+
+    }
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Semantic.vue
@@ -0,0 +1,150 @@
+<template>
+  <!-- IMPORTANT :  It is important that the parent DIV is not the div that needs activating for Semantic.js -->
+  <div id="semantic-playground-body">
+
+    <!-- Accordion -->
+    <div class="ui styled fluid accordion">
+    <div class="title">
+      <i class="dropdown icon"></i>
+      What is a dog?
+    </div>
+    <div class="content">
+      <p class="transition hidden">{{ msg }} <br>A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
+    </div>
+    <div class="title active">
+      <i class="dropdown icon"></i>
+      What kinds of dogs are there?
+    </div>
+    <div class="content active">
+      <p class="transition visible">There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
+    </div>
+    <div class="title">
+      <i class="dropdown icon"></i>
+      How do you acquire a dog?
+    </div>
+    <div class="content">
+      <p>Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
+      <p>A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
+    </div>
+  </div>
+
+  <div id="cards">
+    <!-- Cards -->
+    <div class="ui link centered cards">
+    <div class="card">
+      <div class="image">
+        <img src="https://api.adorable.io/avatars/285/leroy@adorable.io.png">
+      </div>
+      <div class="content">
+        <div class="header">Matt Giampietro</div>
+        <div class="meta">
+          <a>Friends</a>
+        </div>
+        <div class="description">
+          Matthew is an interior designer living in New York.
+        </div>
+      </div>
+      <div class="extra content">
+        <span class="right floated">
+          Joined in 2013
+        </span>
+        <span>
+          <i class="user icon"></i>
+          75 Friends
+        </span>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src="https://api.adorable.io/avatars/285/molly@adorable.io.png">
+      </div>
+      <div class="content">
+        <div class="header">Molly</div>
+        <div class="meta">
+          <span class="date">Coworker</span>
+        </div>
+        <div class="description">
+          Molly is a personal assistant living in Paris.
+        </div>
+      </div>
+      <div class="extra content">
+        <span class="right floated">
+          Joined in 2011
+        </span>
+        <span>
+          <i class="user icon"></i>
+          35 Friends
+        </span>
+      </div>
+    </div>
+    <div class="card">
+      <div class="image">
+        <img src="https://api.adorable.io/avatars/285/elyse@adorable.io.png">
+      </div>
+      <div class="content">
+        <div class="header">Elyse</div>
+        <div class="meta">
+          <a>Coworker</a>
+        </div>
+        <div class="description">
+          Elyse is a copywriter working in New York.
+        </div>
+      </div>
+      <div class="extra content">
+        <span class="right floated">
+          Joined in 2014
+        </span>
+        <span>
+          <i class="user icon"></i>
+          151 Friends
+        </span>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <router-link to="/" class="back-home-link"> Back to Home </router-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'semantic',
+  data () {
+    return {
+      msg: 'This is not the message you were looking for!'
+    }
+  },
+  mounted: function () {
+    console.log("MOUNTED");
+    this.initializeSemantic();
+  },
+  ready: function () {
+    console.log("READY");
+    this.initializeSemantic();
+  },
+  methods: {
+    initializeSemantic: function () {
+      console.log($(this.$el));
+      console.log("INITIALIZING ui.accordions :: ", $(this.$el).find('.ui.accordion') );
+      // Update elements to match what is needed by Semantic.js
+      $(this.$el).find('.ui.accordion').accordion();
+    }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  a {
+    color: #42b983;
+  }
+
+  #cards {
+    margin-top: 60px;
+  }
+
+  .back-home-link {
+    margin-top: 60px;
+  }
+</style>
